a267e27a75955c0990d5c71e671646504028d35a
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-05 14:34:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,11 +7,15 @@ def directors_totals(nds)
   result = {
   }
   puts nds.length
+<<<<<<< HEAD
   
+=======
+>>>>>>> f248da6feed73c5f847b052e2814cc15e25545bd
   nds.length.times do |index|
     #puts nds[index].length
     result[nds[index][:name]] = 0
     puts "MOVIES BY #{nds[index][:name]}"
+<<<<<<< HEAD
     
     nds[index][:movies].length.times do |movies_index|
       puts nds[index][:movies][movies_index][:worldwide_gross]
@@ -21,4 +25,12 @@ def directors_totals(nds)
     puts "\n\n"
   end
   result
+=======
+    nds[index][:movies].length.times do |movies_index|
+      puts nds[index][:movies][movies_index][:title]
+    end
+    puts "\n\n"
+  end
+  puts result
+>>>>>>> f248da6feed73c5f847b052e2814cc15e25545bd
 end
